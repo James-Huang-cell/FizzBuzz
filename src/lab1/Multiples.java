f51bff2a51026820c8f28dbb1fc8d9e86da10097
jameshuang3013 <jameshuang3013@hotmail.com>
2025-09-08 18:30:49 -0400

    add

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -3,6 +3,25 @@ package lab1;
 public class Multiples {
 
     public static int multiples() {
-        return 0;
+
+        int count = 0;
+        for (int i = 1; i < 1000; i++) {
+
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
+                count = count + 1;
+            } else if (divisibleBy3) {
+                count = count + 1;
+            } else if (divisibleBy5) {
+                count = count + 1;
+            } else {
+                count = count + 0;
+            }
+
+        }
     }
 }
