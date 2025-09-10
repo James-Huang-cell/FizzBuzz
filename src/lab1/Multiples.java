1b1b58ff24d679c0d740309c95f694ed8d049de8
jameshuang3013 <jameshuang3013@hotmail.com>
2025-09-10 00:23:06 -0400

    task 3 and 4 changes

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,23 +2,22 @@ package lab1;
 
 public class Multiples {
 
-    // Overloaded method (no arguments, default case)
-    public static int multiples() {
-        return multiples(1000, 3, 5);
+    public static void main(String[] args) {
+
+        System.out.println(multiples());
     }
 
-    // General method
     public static int multiples(int n, int a, int b) {
         int sum = 0;
         for (int i = 1; i < n; i++) {
             if (i % a == 0 || i % b == 0) {
-                sum += i; // add the number itself, not just +1
+                sum += i;
             }
         }
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(multiples()); // should print 233168
+    public static int multiples() {
+        return multiples(1000, 3, 5);
     }
 }
