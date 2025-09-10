9dc34fbc927b9707ab1d9bc0dcdecfe76a00d033
jameshuang3013 <jameshuang3013@hotmail.com>
2025-09-10 00:30:21 -0400

    task 3 and 4 fix

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -3,18 +3,17 @@ package lab1;
 public class Multiples {
 
     public static void main(String[] args) {
-
         System.out.println(multiples());
     }
 
     public static int multiples(int n, int a, int b) {
-        int sum = 0;
+        int count = 0;
         for (int i = 1; i < n; i++) {
             if (i % a == 0 || i % b == 0) {
-                sum += i;
+                count += 1;
             }
         }
-        return sum;
+        return count;
     }
 
     public static int multiples() {
